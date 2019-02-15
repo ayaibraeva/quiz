3580813627da49029440d2e68c7bbd84388e05b2
Ayaibraeva <ayaulymybraeva@gmail.com>
2019-02-15 19:50:20 +0600

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Cat
+    {
+        private string name;
+        private int  age; 
+        public Cat (string name, int age)
+        {
+            this.name = name;
+            this.age = age; 
+        }
+        public string meow()
+        {
+            return name + " "+ "meow"; 
+        }
+       
+       
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat meow = new Cat("alex", 5);
+            Console.WriteLine(meow.meow());
+            Console.ReadKey(); 
+
+        }
+    }
+}
